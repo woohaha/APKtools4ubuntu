920ca9fc4b950f2dfc8afbb7e9843a7554c6f3cf
woohaha <realwoohaha@gmail.com>
2012-11-19 15:08:08 +0800

    it works

--- apkrename.sh
+++ apkrename.sh
@@ -4,10 +4,11 @@
 zenity --error --text="核心解析有文件aapt不存在，請下載";
 exit
 }
-[ -z `$HOME/.bin/aapt d badging $NAUTILUS_SCRIPT_SELECTED_FILE_PATHS` ] && {
+flag=`$HOME/.bin/aapt d badging $NAUTILUS_SCRIPT_SELECTED_FILE_PATHS|head -n1|cut -c1-5`
+[ -z $flag ] && {
 zenity --error --text="不是有效的APK包";
 exit
 }
 pgname=`$HOME/.bin/aapt d badging $NAUTILUS_SCRIPT_SELECTED_FILE_PATHS |grep package|awk '{print $2}'|sed "s/'//g"|cut -c6-`
-vername=`$HOME/.bin/aapt d badging $NAUTILUS_SCRIPT_SELECTED_FILE_PATHS |grep package|awk '{print $3}'|sed "s/'//g"|cut -c13-`
+vername=`$HOME/.bin/aapt d badging $NAUTILUS_SCRIPT_SELECTED_FILE_PATHS |grep package|awk '{print $4}'|sed "s/'//g"|cut -c13-`
 mv $NAUTILUS_SCRIPT_SELECTED_FILE_PATHS ${pgname}_${vername}.apk
